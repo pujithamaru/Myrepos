fcc543cfb1dd66bf79fbde371f3ddbd96e2488a1
Poojitha <pujitha.rissenergy@gmail.com>
2016-01-07 18:05:58 +0530

    Modified commit of MyApp.java

--- MyApp.java
+++ MyApp.java
@@ -8,6 +8,7 @@ public class MyApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
             System.out.println("hello");
+            System.out.println("hai");
 	}
 
 }
